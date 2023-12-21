1c5c326c655578e87d91fca391510fdcf7a1fe5c
Gulg <65360617+GulgDev@users.noreply.github.com>
2023-12-22 04:42:28 +0500

    Update arrows.js

--- arrows.js
+++ arrows.js
@@ -7,7 +7,7 @@ class Arrow {
 }
 
 class Chunk {
-    arrows = new Array(CHUNK_SIZE ** 2).map(() => new Arrow());
+    arrows = new Array(CHUNK_SIZE ** 2).fill().map(() => new Arrow());
     
     constructor(x, y) {
         this.x = x;
