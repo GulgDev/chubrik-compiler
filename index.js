1079936b745d7071fbe55e188f1f96cc183ff9ec
Gulg <65360617+GulgDev@users.noreply.github.com>
2023-12-22 03:57:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import { GameMap } from "./arrows.js";
 
+function compile(asm) {
+    const gameMap = new GameMap();
+    gameMap.setArrow(0, 0, 1, 0, false);
+    return gameMap.save();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const source = document.getElementById("source");
+    const output = document.getElementById("output");
+
+    source.addEventListener("changed", () => {
+        output.value = compile(source.value);
+    });
+});
