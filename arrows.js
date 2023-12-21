427ebb2211e4b6095f28eb393e87c4f294b98c1a
Gulg <65360617+GulgDev@users.noreply.github.com>
2023-12-22 04:12:13 +0500

    Update arrows.js

--- arrows.js
+++ arrows.js
@@ -101,6 +101,15 @@ export class GameMap {
                 buffer[typesCountIndex] = typeCount - 1;
             });
         });
+
+        let binary = "";
+        const bytes = new Uint8Array(buffer);
+        const length = bytes.byteLength;
+        for (let i = 0; i < length; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+
         return buffer;
     }
 }
