ead0d94b35e0adefc4bdc22b4fe4532dbe477184
Hyppoprogramm <65360617+Hyppoprogramm@users.noreply.github.com>
2023-12-23 18:19:45 +0500

    Update compiler

--- asm.js
+++ asm.js
@@ -42,7 +42,38 @@ class Command {
 
 const commands = [
     new Command("mov", [Args.A, Args.ZERO], 0x00),
-    new Command("mov", [Args.A, Args.B], 0x01)
+    new Command("mov", [Args.A, Args.B], 0x01),
+    new Command("mov", [Args.A, Args.C], 0x02),
+    new Command("mov", [Args.A, Args.D], 0x03),
+    new Command("mov", [Args.A, Args.ZERO], 0x04),
+    new Command("mov", [Args.B, Args.A], 0x05),
+    new Command("mov", [Args.C, Args.A], 0x06),
+    new Command("mov", [Args.D, Args.A], 0x07),
+    new Command("and", [Args.A, Args.ZERO], 0x08),
+    new Command("and", [Args.A, Args.B], 0x09),
+    new Command("and", [Args.A, Args.C], 0x0A),
+    new Command("and", [Args.A, Args.D], 0x0B),
+    new Command("and", [Args.A, Args.ZERO], 0x0C),
+    new Command("and", [Args.B, Args.A], 0x0D),
+    new Command("and", [Args.C, Args.A], 0x0E),
+    new Command("and", [Args.D, Args.A], 0x0F),
+    
+    new Command("or", [Args.A, Args.ZERO], 0x10),
+    new Command("or", [Args.A, Args.B], 0x11),
+    new Command("or", [Args.A, Args.C], 0x12),
+    new Command("or", [Args.A, Args.D], 0x13),
+    new Command("or", [Args.A, Args.ZERO], 0x14),
+    new Command("or", [Args.B, Args.A], 0x15),
+    new Command("or", [Args.C, Args.A], 0x16),
+    new Command("or", [Args.D, Args.A], 0x17),
+    new Command("xor", [Args.A, Args.ZERO], 0x18),
+    new Command("xor", [Args.A, Args.B], 0x19),
+    new Command("xor", [Args.A, Args.C], 0x1A),
+    new Command("xor", [Args.A, Args.D], 0x1B),
+    new Command("xor", [Args.A, Args.ZERO], 0x1C),
+    new Command("xor", [Args.B, Args.A], 0x1D),
+    new Command("xor", [Args.C, Args.A], 0x1E),
+    new Command("xor", [Args.D, Args.A], 0x1F),
 ];
 
 class Token {
@@ -51,11 +82,16 @@ class Token {
     static NAME = 0x2;
     static CHAR = 0x3;
     static REGISTER = 0x4;
+    static EOF = 0x5;
 
     constructor(type, value, position) {
         this.type = type;
         this.value = value;
         this.position = position;
+    }
+
+    toString() {
+        return this.type === Token.EOF ? "<eof>" : `'${this.value}'`;
     }
 }
 
@@ -81,6 +117,9 @@ class Tokenizer {
     }
 
     consume() {
+        if (this.offset >= this.source.length)
+            return;
+
         ++this.offset;
         if (this.peekch() === "\n") {
             ++this.line;
@@ -110,7 +149,7 @@ class Tokenizer {
 
         let ch = this.peekch();
         if (ch == null)
-            return null;
+            return new Token(Token.EOF, null, this.position);
         else if (letter.test(ch))
             return this.readName();
         else if (digit.test(ch))
@@ -155,48 +194,67 @@ class Tokenizer {
     }
 }
 
-export class Asm {
-    static parseNumber(token) {
-        if (token.value.length > 2 && token.value[0] === "0")
-            switch (token.value[1].toLowerCase()) {
-                case "x":
-                    return parseInt(token.value.slice(2), 16);
-                case "b":
-                    return parseInt(token.value.slice(2), 2);
-            }
-        return parseInt(token.value);
+class AsmError {
+    constructor(position, message) {
+        this.position = position;
+        this.message = message;
+    }
+}
+
+export class Compiler {
+    bytes = [];
+    errors = [];
+
+    parseNumber(token) {
+        try {
+            if (token.value.length > 2 && token.value[0] === "0")
+                switch (token.value[1].toLowerCase()) {
+                    case "x":
+                        return parseInt(token.value.slice(2), 16);
+                    case "b":
+                        return parseInt(token.value.slice(2), 2);
+                }
+            return parseInt(token.value);
+        } catch {
+            this.errors.push(new AsmError(token.position, `invalid number ${token.value}`));
+        }
     }
 
-    static parseArg(args, token, required=false) {
-        if (token?.type === Token.REGISTER)
-            args.push([registers.indexOf(token.value), token.value]);
-        else if (token?.type === Token.NAME)
-            args.push([Args.REF, token.value]);
-        else if (token?.type === Token.NUMBER) {
+    parseArg(args, token, required=false) {
+        if (token.type === Token.REGISTER)
+            args.push([registers.indexOf(token.value), token.value, token.position]);
+        else if (token.type === Token.NAME)
+            args.push([Args.REF, token.value, token.position]);
+        else if (token.type === Token.NUMBER) {
             const number = this.parseNumber(token);
-            args.push([number === 0 ? Args.ZERO : Args.NUM, number]);
-        } else if (required)
-            throw new Error("unexpected end of argument list");
+            args.push([number === 0 ? Args.ZERO : Args.NUM, number, token.position]);
+        } else if (required) {
+            this.errors.push(new AsmError(token.position, `unexpected ${token}`));
+            return false;
+        }
+        return true;
     }
 
-    static compile(asm) {
-        const tokenizer = new Tokenizer(asm);
+    compile(source) {
+        const tokenizer = new Tokenizer(source);
 
-        const bytes = [];
-        const refs = {};
+        const refs = [];
+        const labels = {};
 
         let token;
-        while ((token = tokenizer.next()) != null) {
-            if (token.type === Token.INSTRUCTION) {
+        while ((token = tokenizer.next()).type !== Token.EOF) {
+            parseCommand: if (token.type === Token.INSTRUCTION) {
                 const instruction = token.value;
+                const { position } = token;
                 const args = [];
 
                 this.parseArg(args, tokenizer.lookahead());
 
                 if (args.length > 0) {
                     tokenizer.next();
-                    while ((token = tokenizer.next())?.type === Token.CHAR && token.value === ",")
-                        this.parseArg(args, tokenizer.next(), true);
+                    while ((token = tokenizer.next()).type === Token.CHAR && token.value === ",")
+                        if (!this.parseArg(args, tokenizer.next(), true))
+                            break parseCommand;
                 }
 
                 let opcode;
@@ -207,22 +265,52 @@ export class Asm {
                     }
 
                 if (opcode == null)
-                    throw new Error("unknown command");
+                    this.errors.push(new AsmError(position, "unknown command"));
 
-                bytes.push(opcode);
+                this.bytes.push(opcode);
 
-                for (const [type, value] of args) {
-                    
+                for (const [type, value, position] of args) {
+                    if (type === Args.NUM)
+                        bytes.push(value && 0xFF);
+                    else if (type === Args.REF) {
+                        const offset = labels[value];
+                        if (offset)
+                            this.bytes.push(offset);
+                        else {
+                            refs.push([value, bytes.length, position]);
+                            this.bytes.push(0x00);
+                        }
+                    }
+                }
+            } else if (token.type === Token.NAME) {
+                const name = token.value;
+                const { position } = token;
+
+                if ((token = tokenizer.next()).type !== Token.CHAR || token.value !== ":") {
+                    this.errors.push(new AsmError(token.position, `unexpected ${token}`));
+                    continue;
+                }
+
+                if (name in labels) {
+                    this.errors.push(new AsmError(position, `label ${name} is already defined`));
+                    continue;
+                }
+
+                labels[name] = bytes.length;
+
+                const refCount = refs.length;
+                for (let i = 0; i < refCount; ++i) {
+                    const ref = refs[i];
+                    if (ref[0] === name) {
+                        bytes[ref[1]] = bytes.length;
+                        refs.splice(i--, 1);
+                    }
                 }
             } else
-                throw new Error("unexpected token");
+                this.errors.push(new AsmError(token.position, `unexpected ${token}`));
         }
 
-        if (Object.values(refs).length > 0)
-            throw new Error("unresolved reference(-s)");
-
-        console.log(bytes);
-
-        return bytes;
+        for (const [name, _offset, position] of refs)
+            this.errors.push(new AsmError(position, `unresolved ${name}`));
     }
 }
