d633b246c6f0b19d0affc8f66164be1bd6931f4a
Hyppoprogramm <65360617+Hyppoprogramm@users.noreply.github.com>
2024-01-26 15:55:07 +0500

    Diskette V2

--- builder.js
+++ builder.js
@@ -1,59 +1,60 @@
 import { GameMap } from "./arrows.js";
 
-const top = new GameMap("AAACAAAAAAANCwYwALQBxAC3AsoAzADOAAoSUABwAJAAsAB0ApQClQGlAFkCiQKKAosCbAKMAm0GjQJuBo4CjwIBEyEBIgE0AkQCZAElArUAxQInAigDuQG6AnsCuwG8Al0GvQG+An8GvwEJFgIAQgBSAGIAcgCCAJIAogCyAMIAAwAEAAUABgAHAAgACQAKAAsADAANAA4ADwANCRICMgR1BpkBmgGbAZwBnQGeAZ8BDAYjAVQCxgKYAUoCOwIsAg4CJAYuBz8HEgA1AgMBRQOGAAQPZQJWAmYDdgGmArYBNwJHAlcDZwF3AKcDOABIAFgAaAATB4UBeQJqAFsATAA9BE4EXwQHAJcBEAypAloDqgJLA6sCPAOsAi0HrQI+BK4CTwSvAgUFegJrAlwCTQZeBm8GAQAAAAkJEAAAAQACAAMABAAUACQANABEAFQAZAB0AIQAlACkALQAxAAOAUAHUQcQAVAEoAITAGAEBQBwBgoCgAJxAMICAQSQArACsQGiArICCwDAAA0AkQAMAJIC");
-const bottom = new GameMap("AAACAAAAAAAHCgEAACAAGAUwAEAAUAAxA0EDUQMJDwIAEgAiAFMAVABVAFYAVwBYAFkAWgBbAFwAXQBeAF8ADAEyAlIADgFCAAUEAQ8GAzgBGQI5ARoCOgEbAjsBHAI8AR0CPQEeAj4BHwI/AQQDBwIXAicCNwELBikAKgArACwALQAuAC8AAQAAAAQKAxAFEgIyAlICEwAwAQkGUAAEABQAJAA0AEQAVAADADEBDQFRAVMH");
+const top = new GameMap("AAACAAAAAAAOCwWgAIUDxQG3BLgHiQEKCcAA0wGUAbQCpQHGA5kBuQCbAZ0BATeRAZIBJAFkASUBZQF1A5UBtQImATYBRgFWAWYBJwE3AUcBVwFnASgBOAFIAVgBaAGIAZgAKQE5AUkBWQFpASoBOgFKAVoBagErATsBSwFbAWsBLAE8AUwBXAFsAS0BPQFNAV0BbQEvAz8DTwNfA28DCRcCABIAIgAyAEIAUgBiAHIAsgDCANIAAwAEAAUABgAHAAgACQAKAAsADAANAA4ADwANA4ICogSkAMQEDAKTAbYDnwEEBcMCpgLHAKgAyACpAwUKNABEAFQANQFFAVUBLgE+AU4BXgFuARIBdACWAA4EhAWGAZoCqgHLAw8AhwIQApcBygO7BgMApwEHAMkEEwG6AqsCAQAAAAkJFwAAAQACAAMABAAFAAYABwAIAAkACgALABsAKwA7AEsAWwBrAHsAiwCbAKsAuwDLAAoMUAKAAoECggJjAoMCZAaEAmUGhQKGAocCeAATB3ACYQBSAEMANARFBFYEZwQNB5ABkQGSAZMBlAGVAZYBmAAQDqACUQOhAkIDogIzA6MCJAekAjUEpQJGBKYCVwSnAgEPsAGxAnICsgGzAlQGtAG1AnYGtgGXArcCuAGpArkCyQIMA0ECMgIjApkCBQZxAmICUwJEBlUGZgZ3BgsDwQDDAMUAxwAOAyUHNgdHB1gH");
+const bottom = new GameMap("AAACAAAAAAAHCgoQADAAUABBA0MDRQNHA0kDSwNNA08DCwFhAxUACQ8CABIAIgAjAiQAJQAmACcAKAApACoAKwAsAC0ALgAvABgIYgNyAIIAYwNzAIMAZAN0AIQABwAEBg4ABQESAAYEAQAWAAEAAAACCQogACEAIgAjACQAJQAmACcACwAbACsACgZBA0MDRQNHAwkCKQJJAw0BKAEqBw==");
+const line = new GameMap("AAACAAAAAAAJCgEQABoFCQECABIABwEEBhQGDgEFARUBEgEGBBYEBQEHARcBDAEIARgBAwEJARkBCwAKBgEECwUMBQ0FDgUPBQEAAAABAQoABQEFAgUDBQQFBQUGBQcFCAUJAhkCCQELABsA");
 
 export function buildDisk(bytes) {
     const gameMap = new GameMap();
 
     const byteCount = bytes.length;
-    const lastByte = byteCount - 1;
 
-    let yOffset = -byteCount * 2 - 18;
+    gameMap.paste(bottom, -2, -2);
 
-    gameMap.paste(top, -2, yOffset);
-
-    yOffset += 13;
+    let y = -3;
 
     for (let i = 0; i < byteCount; ++i) {
-        const byte = bytes[i];
+        let byteA = bytes[i];
+        let byteB = bytes[++i] ?? 0;
 
-        gameMap.setArrow(0, yOffset, 9, 0, false);
-        gameMap.setArrow(18, yOffset, 9, 0, false);
+        for (let x = 9; x < 21; ++x) {
+            gameMap.setArrow(x, y, byteB & 0b10000000 ? 13 : 10, 1, true);
+            byteB <<= 1;
 
-        gameMap.setArrow(-2, yOffset, 10, 0, false);
+            ++x;
 
-        gameMap.setArrow(3, yOffset, 19, 1, false);
-        gameMap.setArrow(4, yOffset, 16, 1, false);
-        gameMap.setArrow(5, yOffset, 13, 1, false);
-
-        let xOffset = 7;
-        for (const bit of byte.toString(2).padStart(8, "0").split("").map((bit) => bit === "1")) {
-            gameMap.setArrow(xOffset, yOffset, bit ? 7 : 1, 1, false);
-            gameMap.setArrow(xOffset, yOffset + 1, 10, 2, false);
-            ++xOffset;
+            gameMap.setArrow(x, y, byteA & 0b10000000 ? 13 : 10, 1, true);
+            byteA <<= 1;
         }
-        ++yOffset;
 
-        gameMap.setArrow(0, yOffset, 9, 0, false);
-        gameMap.setArrow(18, yOffset, 9, 0, false);
+        const bitBA = byteB & 0b10000000;
+        const bitBB = (byteB << 1) & 0b10000000;
+        if (bitBA && bitBB)
+            gameMap.setArrow(21, y, 14, 0, false);
+        else if (bitBA)
+            gameMap.setArrow(21, y, 1, 0, false);
+        else if (bitBB)
+            gameMap.setArrow(21, y, 11, 0, false);
 
-        gameMap.setArrow(2, yOffset, 10, 0, false);
-        if (i < lastByte) {
-            gameMap.setArrow(3, yOffset, 6, 0, false);
-            gameMap.setArrow(4, yOffset, 12, 2, false);
-            gameMap.setArrow(5, yOffset, 10, 3, false);
-        } else {
-            gameMap.setArrow(3, yOffset, 3, 0, false);
-            gameMap.setArrow(5, yOffset, 14, 2, false);
-        }
-        
-        gameMap.setArrow(16, yOffset, 10, 2, false);
+        const bitAA = byteA & 0b10000000;
+        const bitAB = (byteA << 1) & 0b10000000;
+        if (bitAA && bitAB)
+            gameMap.setArrow(22, y, 14, 0, false);
+        else if (bitAA)
+            gameMap.setArrow(22, y, 1, 0, false);
+        else if (bitAB)
+            gameMap.setArrow(22, y, 11, 0, false);
 
-        ++yOffset;
+        --y;
+
+        gameMap.paste(line, -2, y);
+
+        --y;
     }
 
-    gameMap.paste(bottom, -2, yOffset);
+    y -= 12;
+
+    gameMap.paste(top, -2, y);
 
     return gameMap.save();
 }
